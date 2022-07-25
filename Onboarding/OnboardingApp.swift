321e393bce43225adb9979d4f699a97a20c926d7
leilaaua <leila0777@icloud.com>
2022-07-25 17:08:04 +0300

    Initial Commit

---
+++ Onboarding/OnboardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingApp.swift
+//  Onboarding
+//
+//  Created by leila on 25.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
